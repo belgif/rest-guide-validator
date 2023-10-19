01b569a274005e151f4d9cfb2b46859110c57278
Marcel Van Hassel <marcel.vanhassel@smals.be>
2023-10-19 14:48:37 +0200

    Fixed version and assertions in ITs

--- integrationtest/src/it/externalReferencesFail/verify.groovy
+++ integrationtest/src/it/externalReferencesFail/verify.groovy
@@ -3,4 +3,4 @@ String log = new File(basedir, "build.log").text
 assert log.contains("[INFO] Start creation of KieBase: defaultKieBase")
 assert log.contains("[MANDATORY]")
 assert log.contains("At least 1 error in validation !")
-assert log.contains("Logo:Image")
+assert log.contains("[propertyName: Image]\t/components/schemas/Logo")
