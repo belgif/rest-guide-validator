331c95675225c112b7a38efc845d839c2ab8da72
Marcel Van Hassel <marcel.vanhassel@smals.be>
2024-01-04 14:38:18 +0000

    REST-203: Title property of a schema must not be set

---
+++ rules/src/test/java/be/belgium/gcloud/rest/styleguide/validation/rules/oas/TitleShouldNotBeSetTest.java
@@ -0,0 +1,16 @@
+package be.belgium.gcloud.rest.styleguide.validation.rules.oas;
+
+import be.belgium.gcloud.rest.styleguide.validation.rules.AbstractOasRuleTest;
+import org.junit.jupiter.api.Test;
+
+public class TitleShouldNotBeSetTest extends AbstractOasRuleTest {
+    @Test
+    public void testValidOpenApi() {
+        assertNoViolations(callRules("openapi.yaml"));
+    }
+
+    @Test
+    public void testInvalidOpenApi() {
+        assertErrorCount(2 , callRules("openapi_bad.yaml"));
+    }
+}
