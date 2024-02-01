429c692a0feeb639c16a84ae4915d319d74ca1cf
Marcel Van Hassel <marcel.vanhassel@smals.be>
2024-02-01 10:09:55 +0000

    REST-216+REST-217: empty servers array should be valid and improved regex

--- rules/src/test/java/be/belgium/gcloud/rest/styleguide/validation/rules/oas/ServerUrlFormatTest.java
+++ rules/src/test/java/be/belgium/gcloud/rest/styleguide/validation/rules/oas/ServerUrlFormatTest.java
@@ -5,23 +5,55 @@ import org.junit.jupiter.api.Test;
 
 public class ServerUrlFormatTest extends AbstractOasRuleTest {
     @Test
-    public void testValidSwagger() {
-        assertNoViolations(callRules("swagger.yaml"));
+    public void testCorrectMinimalBasepath() {
+        assertNoViolations(callRules("correctMinimalBasepath.yaml"));
     }
-
     @Test
-    public void testInvalidSwagger() {
-        assertViolations(callRules("swagger_bad.yaml"));
+    public void testCorrectLocalhostBasepath() {
+        assertNoViolations(callRules("correctLocalhostBasepath.yaml"));
     }
-
     @Test
-    public void testValidOpenApi() {
-        assertNoViolations(callRules("openapi.yaml"));
+    public void testCorrectLocalhostWithPortBasepath() {
+        assertNoViolations(callRules("correctLocalhostWithPortBasepath.yaml"));
     }
-
     @Test
-    public void testInvalidOpenApi() {
-        assertViolations(callRules("openapi_bad.yaml"));
+    public void testCorrectHttpsLocalhostBasepath() {
+        assertNoViolations(callRules("correctHttpsLocalhostBasepath.yaml"));
     }
-
+    @Test
+    public void testCorrectExternalHostBasepath() {
+        assertNoViolations(callRules("correctExternalHostBasepath.yaml"));
+    }
+    @Test
+    public void testCorrectExternalHostWithPortBasepath() {
+        assertNoViolations(callRules("correctExternalHostWithPortBasepath.yaml"));
+    }
+    @Test
+    public void testCorrectPrefixBasepath() {
+        assertNoViolations(callRules("correctPrefixBasepath.yaml"));
+    }
+    @Test
+    public void testCorrectMultiPrefixBasepath() {
+        assertNoViolations(callRules("correctMultiPrefixBasepath.yaml"));
+    }
+    @Test
+    public void testViolatingInsecureExternalBasepath() {
+        assertViolations(callRules("violatingInsecureExternalBasepath.yaml"));
+    }
+    @Test
+    public void testViolatingNoPathAfterHostBasepath() {
+        assertViolations(callRules("violatingNoPathAfterHostBasepath.yaml"));
+    }
+    @Test
+    public void testViolatingTrailingSlashBasepath() {
+        assertViolations(callRules("violatingTrailingSlashBasepath.yaml"));
+    }
+    @Test
+    public void testViolatingLowerCamelCaseBasepath() {
+        assertViolations(callRules("violatingLowerCamelCaseBasepath.yaml"));
+    }
+    @Test
+    public void testViolatingAbsentVersionBasepath() {
+        assertViolations(callRules("violatingAbsentVersionBasepath.yaml"));
+    }
 }
