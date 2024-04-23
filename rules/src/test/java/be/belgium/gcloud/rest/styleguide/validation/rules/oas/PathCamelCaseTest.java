871c086673e34a64756612b8b26f9ebedf45115a
Marcel Van Hassel <marcel.vanhassel@smals.be>
2024-04-23 14:58:02 +0000

    REST-221: Aligned PathCamelCase with new rules.

--- rules/src/test/java/be/belgium/gcloud/rest/styleguide/validation/rules/oas/PathCamelCaseTest.java
+++ rules/src/test/java/be/belgium/gcloud/rest/styleguide/validation/rules/oas/PathCamelCaseTest.java
@@ -1,8 +1,11 @@
 package be.belgium.gcloud.rest.styleguide.validation.rules.oas;
 
+import be.belgium.gcloud.rest.styleguide.validation.core.OpenApiViolationAggregator;
 import be.belgium.gcloud.rest.styleguide.validation.rules.AbstractOasRuleTest;
 import lombok.Getter;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Getter
 public class PathCamelCaseTest extends AbstractOasRuleTest {
@@ -13,6 +16,13 @@ public class PathCamelCaseTest extends AbstractOasRuleTest {
 
     @Test
     public void testInvalidSwagger() {
-        assertErrorCount(7, callRules("swagger_bad.yaml"));
+        OpenApiViolationAggregator aggregator = callRules("swagger_bad.yaml");
+        int pathSegmentViolations = (int) aggregator.getViolations().stream().filter(violation -> violation.getMessage().contains("Path segments")).count();
+        int pathParamViolations = (int) aggregator.getViolations().stream().filter(violation -> violation.getMessage().contains("Path parameters")).count();
+        int queryParamViolations = (int) aggregator.getViolations().stream().filter(violation -> violation.getMessage().contains("Query parameters")).count();
+        assertEquals(3, pathSegmentViolations);
+        assertEquals(1, pathParamViolations);
+        assertEquals(3, queryParamViolations);
+        assertErrorCount(7, aggregator);
     }
 }
