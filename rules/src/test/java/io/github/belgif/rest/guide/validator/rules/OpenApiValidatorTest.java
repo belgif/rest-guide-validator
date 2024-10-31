3cb6c7381052a5fb365674532c1c8a15d7eb6259
Marcel Van Hassel <marcel.vanhassel@smals.be>
2024-10-31 10:09:37 +0100

    Moved test to rule package

---
+++ rules/src/test/java/io/github/belgif/rest/guide/validator/rules/OpenApiValidatorTest.java
@@ -0,0 +1,17 @@
+package io.github.belgif.rest.guide.validator.rules;
+
+import io.github.belgif.rest.guide.validator.OpenApiValidator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.Objects;
+
+class OpenApiValidatorTest {
+
+    @Test
+    void isOasValid() {
+        var file = new File(Objects.requireNonNull(getClass().getResource("openapi.yaml")).getFile());
+        Assertions.assertTrue(OpenApiValidator.callRules(file, null).isOasValid());
+    }
+}
