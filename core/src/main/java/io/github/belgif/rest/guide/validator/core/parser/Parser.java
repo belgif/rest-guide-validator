68f68880a0cce4c385021f1ec3f21d7435819d79
Marcel Van Hassel <marcel.vanhassel@smals.be>
2025-01-31 15:17:33 +0100

    Improve error message

--- core/src/main/java/io/github/belgif/rest/guide/validator/core/parser/Parser.java
+++ core/src/main/java/io/github/belgif/rest/guide/validator/core/parser/Parser.java
@@ -174,7 +174,7 @@ public class Parser {
         if (invalidSources.isEmpty()) {
             return true;
         } else {
-            invalidSources.forEach(sourceDefinition -> violationReport.addViolation("[unsupported]", "Input files of type OpenApi version 2 / Swagger are not supported. Only OpenAPI 3.0 documents are supported", sourceDefinition.getFileName()));
+            invalidSources.forEach(sourceDefinition -> violationReport.addViolation("[unsupported]", "Input files of type OpenApi version 2.0 / Swagger 2.0 are not supported. Only OpenAPI 3.0 documents are supported", sourceDefinition.getFileName()));
         }
         return false;
     }
