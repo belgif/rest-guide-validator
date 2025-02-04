57d7f9db872f7919036f928df85ebc7635d0dffb
pvdb <peter.vandenbosch@smals.be>
2025-02-04 19:50:05 +0100

    WIP quarkus native support (may break maven plugin)

--- rules/src/main/java/io/github/belgif/rest/guide/validator/OpenApiValidator.java
+++ rules/src/main/java/io/github/belgif/rest/guide/validator/OpenApiValidator.java
@@ -1,9 +1,11 @@
 package io.github.belgif.rest.guide.validator;
 
 import io.github.belgif.rest.guide.validator.core.ViolationReport;
+import jakarta.enterprise.inject.spi.CDI;
 import lombok.extern.slf4j.Slf4j;
 import org.kie.api.KieServices;
-import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieRuntimeBuilder;
+import org.kie.api.runtime.StatelessKieSession;
 
 import java.io.File;
 import java.util.List;
@@ -13,14 +15,22 @@ import java.util.List;
  */
 @Slf4j
 public class OpenApiValidator {
-    private static final KieServices kieServices = KieServices.Factory.get();
-    private static final KieContainer kContainer = kieServices.getKieClasspathContainer();
+    private final StatelessKieSession kSession;
 
-    private OpenApiValidator() {
+    public OpenApiValidator() {
+        StatelessKieSession kieSession;
+        try { // when running in quarkus
+            var kieRuntimeBuilder = CDI.current().select(KieRuntimeBuilder.class).get();
+            kieSession = kieRuntimeBuilder.getKieBase().newStatelessKieSession();
+        } catch(RuntimeException e){
+            var kieServices = KieServices.Factory.get();
+            var kContainer = kieServices.getKieClasspathContainer();
+            kieSession = kContainer.newStatelessKieSession();
+        }
+        this.kSession = kieSession;
     }
 
-    public static ViolationReport callRules(File openApiFile, List<String> excludedFiles) {
-        var kSession = kContainer.newStatelessKieSession();
+    public ViolationReport callRules(File openApiFile, List<String> excludedFiles) {
         return RuleRunner.execute(openApiFile, excludedFiles, kSession);
     }
 }
