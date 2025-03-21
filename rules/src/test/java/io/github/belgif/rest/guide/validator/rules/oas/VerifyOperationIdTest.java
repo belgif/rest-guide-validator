5de6b1189207fec2c9d1db0bbe484c1eded24172
Marcel Van Hassel <marcel.vanhassel@smals.be>
2025-03-21 09:33:04 +0100

    Rule-VerifyOperationId implemented

--- rules/src/test/java/io/github/belgif/rest/guide/validator/rules/oas/VerifyOperationIdTest.java
+++ rules/src/test/java/io/github/belgif/rest/guide/validator/rules/oas/VerifyOperationIdTest.java
@@ -1,16 +1,36 @@
 package io.github.belgif.rest.guide.validator.rules.oas;
 
+import io.github.belgif.rest.guide.validator.core.ViolationReport;
 import io.github.belgif.rest.guide.validator.rules.AbstractOasRuleTest;
 import org.junit.jupiter.api.Test;
 
- class ValuesOfRequiredShouldBeDefinedAsPropertiesTest extends AbstractOasRuleTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class VerifyOperationIdTest extends AbstractOasRuleTest {
+   @Test
+    void testValidOpenApi() {
+       assertNoViolations(callRules("openapi.yaml"));
+   }
+
+   @Test
+    void testOperationIdNotSpecified() {
+       ViolationReport violationReport = callRules("operationIdNotSpecified.yaml");
+       assertErrorCount(1, violationReport);
+       assertEquals("OperationId not specified.", violationReport.getViolations().get(0).getMessage());
+   }
+
     @Test
-     void testValidOpenApi() {
-        assertNoViolations(callRules("openapi.yaml"));
+    void testOperationIdNotInLowerCamelCase() {
+        ViolationReport violationReport = callRules("operationIdNotInLowerCamelCase.yaml");
+        assertErrorCount(1, violationReport);
+        assertEquals("OperationId not lowerCamelCase: MyPath", violationReport.getViolations().get(0).getMessage());
     }
 
     @Test
-     void testInvalidOpenApi() {
-        assertErrorCount(3, callRules("openapi_bad.yaml"));
+    void testDuplicateOperationId() {
+        ViolationReport violationReport = callRules("duplicateOperationId.yaml");
+        assertErrorCount(1, violationReport);
+        assertTrue(violationReport.getViolations().get(0).getMessage().contains("OperationId not unique, present in: "));
     }
 }
