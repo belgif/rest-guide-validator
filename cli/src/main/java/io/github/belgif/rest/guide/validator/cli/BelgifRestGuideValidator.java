9c96d0d2f7137bfc0f99afea1d59a6c492fb02c8
Marcel van Hassel <marcel.vanhassel@smals.be>
2025-07-24 08:50:52 +0200

    Added try-catch-finally block to not let window close on exceptions

--- cli/src/main/java/io/github/belgif/rest/guide/validator/cli/BelgifRestGuideValidator.java
+++ cli/src/main/java/io/github/belgif/rest/guide/validator/cli/BelgifRestGuideValidator.java
@@ -11,10 +11,18 @@ public class BelgifRestGuideValidator {
     private static final Logger log = LoggerFactory.getLogger(BelgifRestGuideValidator.class);
 
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new BelgifRestGuideCli()).execute(args);
-        log.info("\nPress Enter to exit...");
-        new Scanner(System.in).nextLine();
-        System.exit(exitCode);
+        int exitCode = 1;
+        try {
+            exitCode = new CommandLine(new BelgifRestGuideCli()).execute(args);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        } finally {
+            try (Scanner sc = new Scanner(System.in)) {
+                log.info("\nPress Enter to exit...");
+                sc.nextLine();
+            }
+            System.exit(exitCode);
+        }
     }
 
 }
