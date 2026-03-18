c672947dfd579a65427834ac4d496f3bdfab5c63
Marcel van Hassel <marcel.vanhassel@smals.be>
2026-03-18 19:19:30 +0100

    Add bulk testcases

--- rules/src/test/java/io/github/belgif/rest/guide/validator/rules/AbstractOasRuleTest.java
+++ rules/src/test/java/io/github/belgif/rest/guide/validator/rules/AbstractOasRuleTest.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,6 +26,34 @@ public abstract class AbstractOasRuleTest {
         var openApiFile = new File(getClass().getResource(lowerCaseFirstStripTestSuffix(getClass().getSimpleName()) + "/" + fileName).getFile());
         var validator = new OpenApiSingleRuleValidator(ruleUrl.getFile());
         return validator.callRule(openApiFile, null);
+    }
+
+    /*
+    This method can be used during development to test a certain rule against multiple openapi files. Will search folders recursively.
+    Expects an absolute file path to the folder
+     */
+    protected Map<String, ViolationReport> callRulesOnFolder(String folder) {
+        String ruleFileName = getRulesFile(getClass().getSimpleName());
+        var ruleUrl = getClass().getResource(ruleFileName);
+        if (ruleUrl == null) {
+            throw new RuntimeException("Could not find file " + ruleFileName);
+        }
+        var validator = new OpenApiSingleRuleValidator(ruleUrl.getFile());
+        List<File> openapiFiles = findOpenApiFiles(folder);
+        log.info("Found {} openapi files", openapiFiles.size());
+        Map<String, ViolationReport> reports = new HashMap<>();
+        for (File openapi : openapiFiles) {
+            try {
+                reports.put(openapi.getAbsolutePath(), validator.callRule(openapi, null));
+            } catch (Exception e) {
+                log.error("File: {} got an exception with the validator", openapi.getAbsolutePath());
+                reports.put(openapi.getAbsolutePath(), null);
+            }
+        }
+        log.info("{} reports were made", reports.size());
+        log.info("There were {} files with violations on this rule.", reports.entrySet().stream().filter(e -> !e.getValue().isOasValid()).count());
+        log.info("{} files were compliant with this rule", reports.entrySet().stream().filter(e -> e.getValue().isOasValid()).count());
+        return reports;
     }
 
     public static void assertNoViolations(ViolationReport validationResult) {
@@ -51,6 +80,20 @@ public abstract class AbstractOasRuleTest {
 
     private static String lowerCaseFirstStripTestSuffix(String s) {
         return s.substring(0, 1).toLowerCase() + s.substring(1, s.length() - 4);
+    }
+
+    private static List<File> findOpenApiFiles(String folder) {
+        List<File> openApiFiles = new ArrayList<>();
+        File baseFolder = new File(folder);
+        for (File child : Objects.requireNonNull(baseFolder.listFiles())) {
+            if (child.isDirectory()) {
+                openApiFiles.addAll(findOpenApiFiles(child.getAbsolutePath()));
+            }
+            if (child.getName().contains("openapi.yaml")) {
+                openApiFiles.add(child);
+            }
+        }
+        return openApiFiles;
     }
 
 }
--- rules/src/test/java/io/github/belgif/rest/guide/validator/rules/oas/OneOfSchemaTest.java
+++ rules/src/test/java/io/github/belgif/rest/guide/validator/rules/oas/OneOfSchemaTest.java
@@ -8,20 +8,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OneOfSchemaTest extends AbstractOasRuleTest {
 
-   @Test
+    @Test
     void testOneOfWithRequiredConstraint() {
-       assertNoViolations(callRules("oneOfWithRequiredConstraint.yaml"));
-   }
+        assertNoViolations(callRules("oneOfWithRequiredConstraint.yaml"));
+    }
 
-   @Test
+    @Test
     void testOneOfWithFullObjectConstraint() {
-       assertErrorCount(1, callRules("oneOfWithFullObjectConstraint.yaml"));
-   }
+        assertErrorCount(1, callRules("oneOfWithFullObjectConstraint.yaml"));
+    }
 
-   @Test
+    @Test
     void testOneOfWithEnumConstraint() {
-       assertNoViolations(callRules("oneOfWithEnumConstraint.yaml"));
-   }
+        assertNoViolations(callRules("oneOfWithEnumConstraint.yaml"));
+    }
 
     @Test
     void testOneOfWithEnumAndDifferentTypeConstraint() {
@@ -30,22 +30,22 @@ class OneOfSchemaTest extends AbstractOasRuleTest {
 
     @Test
     void testOneOfWithMultipleObjectTypes() {
-       assertErrorCount(1, callRules("oneOfWithMultipleObjectTypes.yaml"));
+        assertErrorCount(1, callRules("oneOfWithMultipleObjectTypes.yaml"));
     }
 
     @Test
     void testSubSchemasWithTypeObjectAndDiscriminator() {
-       assertNoViolations(callRules("subSchemasWithTypeObject.yaml"));
+        assertNoViolations(callRules("subSchemasWithTypeObject.yaml"));
     }
 
     @Test
     void testSubSchemasWithDiscriminatorInParent() {
-       assertErrorCount(1, callRules("subSchemasWithDiscriminatorInParent.yaml"));
+        assertErrorCount(1, callRules("subSchemasWithDiscriminatorInParent.yaml"));
     }
 
     @Test
     void testOneOfWithExtraProperty() {
-       assertErrorCount(1, callRules("oneOfWithExtraProperty.yaml"));
+        assertErrorCount(1, callRules("oneOfWithExtraProperty.yaml"));
     }
 
     @Test
@@ -55,8 +55,8 @@ class OneOfSchemaTest extends AbstractOasRuleTest {
 
     @Test
     void testNonCombinableSubSchemas() {
-       ViolationReport report = callRules("nonCombinableOneOfSchemas.yaml");
-       assertErrorCount(1, report);
-       assertEquals("All subschemas should comply with one of the allowed uses for a oneOf schema.", report.getViolations().get(0).getMessage());
+        ViolationReport report = callRules("nonCombinableOneOfSchemas.yaml");
+        assertErrorCount(1, report);
+        assertEquals("All subschemas should comply with one of the allowed uses for a oneOf schema.", report.getViolations().get(0).getMessage());
     }
 }
