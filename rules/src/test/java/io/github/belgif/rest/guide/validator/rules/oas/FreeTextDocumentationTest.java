2b87f0c4766e18297d154930925643d6741351f3
Mattias Doevenspeck <mattias.doevenspeck@smals.be>
2026-04-08 11:36:57 +0200

    renamed to FreeTextDocumentation

--- rules/src/test/java/io/github/belgif/rest/guide/validator/rules/oas/FreeTextDocumentationTest.java
+++ rules/src/test/java/io/github/belgif/rest/guide/validator/rules/oas/FreeTextDocumentationTest.java
@@ -3,7 +3,7 @@ package io.github.belgif.rest.guide.validator.rules.oas;
 import io.github.belgif.rest.guide.validator.rules.AbstractOasRuleTest;
 import org.junit.jupiter.api.Test;
 
-class GeneralDocumentationTest extends AbstractOasRuleTest {
+class FreeTextDocumentationTest extends AbstractOasRuleTest {
 
 
     @Test
