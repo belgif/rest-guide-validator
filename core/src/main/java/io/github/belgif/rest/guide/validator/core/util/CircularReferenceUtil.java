7bde414c102979d2cada07e7e8f47e57ada9057e
Marcel van Hassel <marcel.vanhassel@smals.be>
2026-07-10 16:11:29 +0200

    Basic refactoring done

--- core/src/main/java/io/github/belgif/rest/guide/validator/core/util/CircularReferenceUtil.java
+++ core/src/main/java/io/github/belgif/rest/guide/validator/core/util/CircularReferenceUtil.java
@@ -5,11 +5,10 @@ import io.github.belgif.rest.guide.validator.core.model.SchemaDefinition;
 import io.github.belgif.rest.guide.validator.core.parser.Parser;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.openapi.models.Reference;
 
 import java.util.ArrayList;
-import java.util.EnumSet;
 import java.util.List;
-import java.util.Set;
 
 @Slf4j
 public class CircularReferenceUtil {
@@ -17,16 +16,9 @@ public class CircularReferenceUtil {
     private CircularReferenceUtil() {
     }
 
-    private static final Set<RefType> SAFE_REFS = EnumSet.of(
-            RefType.PROPERTY,
-            RefType.ITEMS,
-            RefType.ADDITIONAL_PROPERTIES,
-            RefType.DISCRIMINATOR //Not considered safe when it's the only reference.
-    );
-
     public static void validateCircularReferences(Parser.ParserResult result) {
         for (OpenApiDefinition<?> def : result.getAllDefinitions()) {
-            if (containsUnsafeCycle(def, new ArrayList<>(), new ArrayList<>())) {
+            if (containsUnsafeCycle(def, new ArrayList<>(), result)) {
                 log.error("{} contains a circular reference to itself", def.getPrintableJsonPointer());
                 result.setParsingValid(false);
                 return;
@@ -34,106 +26,111 @@ public class CircularReferenceUtil {
         }
     }
 
-    private static boolean containsUnsafeCycle(OpenApiDefinition<?> current, List<OpenApiDefinition<?>> visited, List<RefType> refTypePath) {
-        int cycleStart = visited.indexOf(current);
-        if (cycleStart >= 0) {
-            List<RefType> refTypeCyclePath = refTypePath.subList(cycleStart, refTypePath.size());
-
-            return isUnsafe(refTypeCyclePath);
+    private static boolean containsUnsafeCycle(OpenApiDefinition<?> def, List<Ref> visited, Parser.ParserResult result) {
+        if (visitedContainsDefinitionViaUnsafeRef(def, visited)) {
+            return true;
         }
-
-        visited.add(current);
-
-        for (OpenApiDefinition<?> ref : current.getReferencedBy()) {
-            RefType type = getRefType(ref);
-            refTypePath.add(type);
-
-            if (containsUnsafeCycle(findReferencingDefinition(ref), visited, refTypePath)) {
-                return true;
-            }
-
-            refTypePath.remove(refTypePath.size() - 1);
+        if (visited.stream().anyMatch(r -> r.refFrom().equals(def))) {
+            // In case there is a legit circular reference, the infinite loop should stop.
+            return false;
         }
-
-        visited.remove(visited.size() - 1);
-        return false;
+        if (!(def.getModel() instanceof Reference)) {
+            return false;
+        }
+        if (def instanceof SchemaDefinition schemaDefinition) {
+            // Return if one of these options is true:
+            return directRefContainsUnsafeCycle(def, visited, result) ||
+                    discriminatorRefContainsUnsafeCycle(schemaDefinition, visited, result) ||
+                    allOfRefContainsUnsafeCycle(schemaDefinition, visited, result) ||
+                    oneOfRefContainsUnsafeCycle(schemaDefinition, visited, result) ||
+                    anyOfRefContainsUnsafeCycle(schemaDefinition, visited, result) ||
+                    notRefContainsUnsafeCycle(schemaDefinition, visited, result);
+        } else {
+            return directRefContainsUnsafeCycle(def, visited, result);
+        }
     }
 
-    /*
-    Finds definition that references something, so for example the schema with a oneOf instead of the index of the oneOf with $ref.
-    If the schema contains a discriminator it is always returned.
-     */
-    private static OpenApiDefinition<?> findReferencingDefinition(OpenApiDefinition<?> ref) {
-        if (ref.getDefinitionType() == OpenApiDefinition.DefinitionType.TOP_LEVEL || !ref.getParent().getClass().isInstance(ref)) {
-            return ref;
-        }
-        if (ref instanceof SchemaDefinition schemaRef && (
-                schemaRef.isInlineSchemaOfProperty() ||
-                        schemaRef.getModel().getDiscriminator() != null
-        )) {
-            return schemaRef;
-        }
-        return findReferencingDefinition(ref.getParent());
+    private static boolean visitedContainsDefinitionViaUnsafeRef(OpenApiDefinition<?> def, List<Ref> visited) {
+        return visited.stream().anyMatch(r -> r.refFrom().equals(def))
+                && (
+                visited.stream().allMatch(r -> r.refType() == RefType.DISCRIMINATOR) ||
+                        visited.stream().noneMatch(r -> r.refType() == RefType.DISCRIMINATOR)
+        );
     }
 
-    private static boolean isUnsafe(List<RefType> refTypes) {
-        return refTypes.stream().noneMatch(SAFE_REFS::contains) ||
-                refTypes.stream().allMatch(r -> r == RefType.DISCRIMINATOR);
+    private static boolean notRefContainsUnsafeCycle(SchemaDefinition schemaDefinition, List<Ref> visited, Parser.ParserResult result) {
+        if (schemaDefinition.getModel().getNot() == null) {
+            return false;
+        }
+        OpenApiDefinition<?> referenced = result.resolve(schemaDefinition.getModel().getNot());
+        return callNextCycle(schemaDefinition, referenced, RefType.NOT, visited, result);
     }
 
-    private static RefType getRefType(OpenApiDefinition<?> ref) {
-        int index = findReferencingDefinition(ref).getJsonPointer().splitSegments().size();
-        List<String> relativeJsonPointerSegments = ref.getJsonPointer().splitSegments().subList(index, ref.getJsonPointer().splitSegments().size());
-        // direct ref or discriminator
-        if (relativeJsonPointerSegments.isEmpty()) {
-            if (ref.hasReference()) {
-                return RefType.REF;
-            }
-            if (ref instanceof SchemaDefinition schemaRef && schemaRef.getModel().getDiscriminator() != null) {
-                return RefType.DISCRIMINATOR;
-            }
-            throw new IllegalStateException("No valid reference type found.");
+    private static boolean anyOfRefContainsUnsafeCycle(SchemaDefinition schemaDefinition, List<Ref> visited, Parser.ParserResult result) {
+        if (schemaDefinition.getModel().getAnyOf() == null || schemaDefinition.getModel().getAnyOf().isEmpty()) {
+            return false;
         }
-        return RefType.fromSegment(relativeJsonPointerSegments.get(findRelevantSegmentIndex(relativeJsonPointerSegments, ref)));
+        return schemaDefinition.getModel().getAnyOf().stream()
+                .map(result::resolve)
+                .anyMatch(r -> callNextCycle(schemaDefinition, r, RefType.ONE_OF, visited, result));
     }
 
-    private static int findRelevantSegmentIndex(List<String> segments, OpenApiDefinition<?> ref) {
-        String lastSegment = segments.get(segments.size() - 1);
-        if (!lastSegment.equals(RefType.ITEMS.getSegment()) && !lastSegment.equals(RefType.ADDITIONAL_PROPERTIES.getSegment())) {
-            return segments.size() - 2;
+
+    private static boolean oneOfRefContainsUnsafeCycle(SchemaDefinition schemaDefinition, List<Ref> visited, Parser.ParserResult result) {
+        if (schemaDefinition.getModel().getOneOf() == null || schemaDefinition.getModel().getOneOf().isEmpty()) {
+            return false;
         }
-        if (lastSegment.equals(RefType.ITEMS.getSegment()) && ref.getParent() != null && ref.getParent() instanceof SchemaDefinition schema && schema.getModel().getItems() != null) {
-            return segments.size() - 1;
+        return schemaDefinition.getModel().getOneOf().stream()
+                .map(result::resolve)
+                .anyMatch(r -> callNextCycle(schemaDefinition, r, RefType.ONE_OF, visited, result));
+    }
+
+    private static boolean allOfRefContainsUnsafeCycle(SchemaDefinition schemaDefinition, List<Ref> visited, Parser.ParserResult result) {
+        if (schemaDefinition.getModel().getAllOf() == null || schemaDefinition.getModel().getAllOf().isEmpty()) {
+            return false;
         }
-        return (lastSegment.equals(RefType.ADDITIONAL_PROPERTIES.getSegment()) && ref.getParent() != null && ref.getParent() instanceof SchemaDefinition schema && schema.getModel().getAdditionalPropertiesSchema() != null) ? segments.size() - 1 : segments.size() - 2;
+        return schemaDefinition.getModel().getAllOf().stream()
+                .map(result::resolve)
+                .anyMatch(r -> callNextCycle(schemaDefinition, r, RefType.ALL_OF, visited, result));
+    }
+
+    private static boolean discriminatorRefContainsUnsafeCycle(SchemaDefinition schemaDefinition, List<Ref> visited, Parser.ParserResult result) {
+        if (schemaDefinition.getModel().getDiscriminator() == null || schemaDefinition.getModel().getDiscriminator().getMapping() == null) {
+            return false;
+        }
+        return schemaDefinition.getModel().getDiscriminator().getMapping().values().stream()
+                .map(m -> result.resolveDiscriminatorMapping(schemaDefinition, m).orElseThrow(() -> new IllegalStateException("Reference does not exist but somehow was not catched before")))
+                .anyMatch(r -> callNextCycle(schemaDefinition, r, RefType.DISCRIMINATOR, visited, result));
+    }
+
+    private static boolean directRefContainsUnsafeCycle(OpenApiDefinition<?> def, List<Ref> visited, Parser.ParserResult result) {
+        if (!def.hasReference()) {
+            return false;
+        }
+        OpenApiDefinition<?> referenced = result.resolve(def.getModel());
+        return callNextCycle(def, referenced, RefType.REF, visited, result);
+    }
+
+    private static boolean callNextCycle(OpenApiDefinition<?> def, OpenApiDefinition<?> referenced, RefType refType, List<Ref> visited, Parser.ParserResult result) {
+        List<Ref> branchedVisited = new ArrayList<>(visited);
+        branchedVisited.add(new Ref(def, refType));
+        return containsUnsafeCycle(referenced, branchedVisited, result);
+    }
+
+    private record Ref(OpenApiDefinition<?> refFrom, RefType refType) {
     }
 
     @Getter
     private enum RefType {
-        REF(""),
-        ALL_OF("allOf"),
-        ONE_OF("oneOf"),
-        ANY_OF("anyOf"),
-        NOT("not"),
-        DISCRIMINATOR(""),
-        PROPERTY("properties"),
-        ITEMS("items"),
-        ADDITIONAL_PROPERTIES("additionalProperties");
-
-        private final String segment;
-
-        RefType(String segment) {
-            this.segment = segment;
-        }
-
-        public static RefType fromSegment(String segment) {
-            for (RefType value : values()) {
-                if (value.segment.equals(segment)) {
-                    return value;
-                }
-            }
-            throw new IllegalArgumentException("Unknown segment: " + segment);
-        }
+        REF(),
+        ALL_OF(),
+        ONE_OF(),
+        ANY_OF(),
+        NOT(),
+        DISCRIMINATOR(),
+        PROPERTY(),
+        ITEMS(),
+        ADDITIONAL_PROPERTIES()
     }
 
 }
