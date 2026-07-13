6169bbc73764d8f0be3fd8808483e55e71bb61d6
pvdb <peter.vandenbosch@smals.be>
2026-07-13 14:38:12 +0200

    - separate discriminator circular validation - log correct circular definition

--- core/src/main/java/io/github/belgif/rest/guide/validator/core/util/CircularReferenceUtil.java
+++ core/src/main/java/io/github/belgif/rest/guide/validator/core/util/CircularReferenceUtil.java
@@ -4,7 +4,6 @@ import io.github.belgif.rest.guide.validator.core.model.OpenApiDefinition;
 import io.github.belgif.rest.guide.validator.core.model.SchemaDefinition;
 import io.github.belgif.rest.guide.validator.core.parser.Parser;
 import lombok.extern.slf4j.Slf4j;
-import org.eclipse.microprofile.openapi.models.Reference;
 import org.eclipse.microprofile.openapi.models.media.Schema;
 
 import java.util.ArrayList;
@@ -18,13 +17,40 @@ public class CircularReferenceUtil {
 
     public static void validateCircularReferences(Parser.ParserResult result) {
         for (OpenApiDefinition<?> def : result.getAllDefinitions()) {
-            if (containsUnsafeCycle(def, new ArrayList<>(), result)) {
-                log.error("{} contains a circular reference to itself", def.getPrintableJsonPointer());  //TODO: not necessarily def anymore, but can be nested/referenced schema
+            OpenApiDefinition<?> circularDefinition = findCircularReference(def, new ArrayList<>(), result);
+            if (circularDefinition != null) {
+                log.error("{} contains a circular reference to itself", circularDefinition.getPrintableJsonPointer());
                 result.setParsingValid(false);
                 return;
             }
+            if(def instanceof SchemaDefinition) {
+                SchemaDefinition circularDiscriminatorDefinition = findCircularDiscriminator((SchemaDefinition) def, List.of(), result);
+                if (circularDiscriminatorDefinition != null) {
+                    log.error("{} contains a circular reference to itself via discriminators", circularDiscriminatorDefinition.getPrintableJsonPointer());
+                    result.setParsingValid(false);
+                    return;
+                }
+            }
         }
     }
+
+    private static SchemaDefinition findCircularDiscriminator(SchemaDefinition def, List<SchemaDefinition> visited, Parser.ParserResult result) {
+        if(visited.contains(def)) {
+            return def;
+        }
+        List<SchemaDefinition> refs = new ArrayList<>();
+        addDiscriminatorRefs(refs, def, result);
+        for (SchemaDefinition ref : refs) {
+            List<SchemaDefinition> nextVisit = new ArrayList<>(visited);
+            nextVisit.add(def);
+            SchemaDefinition circularDefinition = findCircularDiscriminator(ref, nextVisit, result);
+            if(circularDefinition != null) {
+                return circularDefinition;
+            }
+        }
+        return null;
+    }
+
 
     /**
      *
@@ -33,81 +59,56 @@ public class CircularReferenceUtil {
      * @param result
      * @return  Whether there's an unsafe circular reference concerning 'def' or one of the definitions it references
      */
-    private static boolean containsUnsafeCycle(OpenApiDefinition<?> def, List<Visit> visited, Parser.ParserResult result) {
-        if (isUnsafeCycle(def, visited)) {
-            return true;
+    private static OpenApiDefinition<?> findCircularReference(OpenApiDefinition<?> def, List<OpenApiDefinition<?>> visited, Parser.ParserResult result) {
+        if (visited.contains(def)) {
+            return def;
         }
-        if (visited.stream().anyMatch(r -> r.source().equals(def))) {
-            // In case there is a legit circular reference, the infinite loop should stop.
-            return false;
-        }
-        if (!(def.getModel() instanceof Reference)) {  //TODO: check if it's a type that can be a reference; why this check? instead of def.getModel() instanceof def.getClass()
-            return false;
-        }
-        for (Ref ref : getOutgoingRefs(def, result)) {  // TODO: not always refs, can also be subschemas
-            List<Visit> nextVisit = new ArrayList<>(visited);
-            nextVisit.add(new Visit(def, ref.discriminator));
+        for (OpenApiDefinition<?> ref : getUsedSchemas(def, result)) {
+            List<OpenApiDefinition<?>> nextVisit = new ArrayList<>(visited);
+            nextVisit.add(def);
 
-            if (containsUnsafeCycle(ref.target(), nextVisit, result)) {
-                return true;
+            OpenApiDefinition<?> circularDefinition = findCircularReference(ref, nextVisit, result);
+            if (circularDefinition != null) {
+                return circularDefinition;
             }
         }
-        return false;
+        return null;
     }
 
-    private static List<Ref> getOutgoingRefs(OpenApiDefinition<?> definition, Parser.ParserResult result) {
-        List<Ref> refs = new ArrayList<>();
+    private static List<OpenApiDefinition<?>> getUsedSchemas(OpenApiDefinition<?> definition, Parser.ParserResult result) {
+        List<OpenApiDefinition<?>> usedSchemas = new ArrayList<>();
 
         if (definition.hasReference()) {
-            refs.add(new Ref(result.resolve(definition.getModel()), false));
+            usedSchemas.add(result.resolve(definition.getModel()));
         }
 
         if (definition instanceof SchemaDefinition schemaDefinition) {
-            addDiscriminatorRefs(refs, schemaDefinition, result);
-            addSchemaCollectionRefs(refs, schemaDefinition.getModel().getAllOf(), result);
-            addSchemaCollectionRefs(refs, schemaDefinition.getModel().getOneOf(), result);
-            addSchemaCollectionRefs(refs, schemaDefinition.getModel().getAnyOf(), result);
+            addSchemaCollectionRefs(usedSchemas, schemaDefinition.getModel().getAllOf(), result);
+            addSchemaCollectionRefs(usedSchemas, schemaDefinition.getModel().getOneOf(), result);
+            addSchemaCollectionRefs(usedSchemas, schemaDefinition.getModel().getAnyOf(), result);
 
             if (schemaDefinition.getModel().getNot() != null) {
-                refs.add(new Ref(result.resolve(schemaDefinition.getModel().getNot()), false));
+                usedSchemas.add(result.resolve(schemaDefinition.getModel().getNot()));
             }
         }
 
-        return refs;
+        return usedSchemas;
     }
 
-    private static boolean isUnsafeCycle(OpenApiDefinition<?> def, List<Visit> visited) {
-        return visited.stream().anyMatch(r -> r.source().equals(def))
-                && (
-                visited.stream().allMatch(Visit::discriminator) ||
-                        visited.stream().noneMatch(Visit::discriminator)
-        );
-    }
-
-
-    private static void addSchemaCollectionRefs(List<Ref> refs, List<Schema> schemaDefinitionList, Parser.ParserResult result) {
+    private static void addSchemaCollectionRefs(List<OpenApiDefinition<?>> usedSchemas, List<Schema> schemaDefinitionList, Parser.ParserResult result) {
         if (schemaDefinitionList == null || schemaDefinitionList.isEmpty()) {
             return;
         }
-        refs.addAll(schemaDefinitionList.stream().map(result::resolve).map(r -> new Ref(r, false)).toList());
+        usedSchemas.addAll(schemaDefinitionList.stream().map(result::resolve).toList());
     }
 
-    private static void addDiscriminatorRefs(List<Ref> refs, SchemaDefinition schemaDefinition, Parser.ParserResult result) {
+    private static void addDiscriminatorRefs(List<SchemaDefinition> refs, SchemaDefinition schemaDefinition, Parser.ParserResult result) {
         if (schemaDefinition.getModel().getDiscriminator() == null || schemaDefinition.getModel().getDiscriminator().getMapping() == null) {
             return;
         }
         refs.addAll(schemaDefinition.getModel().getDiscriminator().getMapping().values().stream()
                 .map(m -> result.resolveDiscriminatorMapping(schemaDefinition, m).orElseThrow(() -> new IllegalStateException("Reference does not exist but somehow was not catched before")))
-                .map(r -> new Ref(r, true))
                 .toList());
-    }
-
-    private record Ref(
-            OpenApiDefinition<?> target,
-            boolean discriminator) {
-    }
-
-    private record Visit(OpenApiDefinition<?> source, boolean discriminator) {
     }
 
 }
